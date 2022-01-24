01d4425f3909e29e389d4b8c22392c3bd2393189
Claude Grondin <claude.grondin@telus.com>
2022-01-24 10:56:36 -0500

    feat: add corp-proxy oh-my-zsh plugin

---
+++ corp-proxy.plugin.zsh
@@ -0,0 +1,7 @@
+function proxy_on() {
+  source ./enable_proxy.sh
+}
+
+function proxy_off() {
+  source ./disable_proxy.sh
+}
---
+++ disable_proxy.sh
@@ -0,0 +1,10 @@
+# /bin/sh
+unset HTTP_PROXY_HOST
+unset HTTP_PROXY_PORT
+unset NO_PROXY
+unset HTTP_PROXY
+unset HTTPS_PROXY
+unset GIT_SSH_COMMAND
+
+echo $GIT_SSH_COMMAND
+echo 'corporate proxy disabled'
